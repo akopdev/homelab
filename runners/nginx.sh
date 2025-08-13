841b0469ab2588c7838389fd25e8a2ba9fc4b68a
Akop Kesheshyan <hello@akop.dev>
2025-08-13 08:53:38 +0200

    Migrate to podman systemd

--- runners/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-run_container  docker.io/library/nginx:latest \
--p 80:80 \
--p 443:443 \
--v /etc/homelab/configs/nginx.conf:/etc/nginx/conf.d/default.conf:ro
--- runners/postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-run_container docker.io/library/postgres:15-alpine \
-  -p 5432:5432 \
-  -e  POSTGRES_DB=forgejo \
-  -e  POSTGRES_USER=forgejo \
-  -e  POSTGRES_PASSWORD=changeme \
-  -v postgres-data:/var/lib/postgresql/data
